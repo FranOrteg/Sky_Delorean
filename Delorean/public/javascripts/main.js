647bfe66e342b4a1f6112a23e34451eda49c3baa
FranOrteg <ortegfran@gmail.com>
2024-06-20 12:25:00 +0200

    Main Refactor

--- Delorean/public/javascripts/main.js
+++ Delorean/public/javascripts/main.js
@@ -1,17 +1,37 @@
-// public/javascripts/main.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('/api/user')
-        .then(response => response.json())
-        .then(users => {
-            const usersContainer = document.getElementById('users-container');
-            users.forEach(user => {
-                const userDiv = document.createElement('div');
-                userDiv.textContent = `ID: ${user.contact_id}, Name: ${user.Name}, Email: ${user.Email} `;
-                usersContainer.appendChild(userDiv);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching users:', error);
-        });
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const users = await fetchUsers();
+        renderUsers(users);
+        renderUserData(users);
+        // Aquí puedes llamar a más funciones para añadir más elementos
+        createInputElement();
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
 });
+
+async function fetchUsers() {
+    const response = await fetch('/api/user');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+}
+
+function renderUsers(users) {
+    const usersContainer = document.getElementById('users-container');
+    users.forEach(user => {
+        const userDiv = document.createElement('div');
+        userDiv.textContent = `ID: ${user.contact_id}, Name: ${user.Name}, Email: ${user.Email} `;
+        usersContainer.appendChild(userDiv);
+    });
+}
+
+function renderUserData(users) {
+    const userData = document.getElementById('users-data');
+    users.forEach(user => {
+        const dataDiv = document.createElement('div');
+        dataDiv.textContent = `Country: ${user.Country}, BagMinutes: ${user.bagMinutes} `;
+        userData.appendChild(dataDiv);
+    });
+}
